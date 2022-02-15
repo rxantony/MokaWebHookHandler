88d56864c91fa0b1e0071396ccd963678ba3783a
Rudi Antony <rudi.antony@mekari.com>
2022-02-15 12:56:49 +0700

    change sourceName as a final field

--- common/src/main/java/com/mekari/mokaaddons/webhookhandler/common/consumer/AbstractDeadLetterConsumer.java
+++ common/src/main/java/com/mekari/mokaaddons/webhookhandler/common/consumer/AbstractDeadLetterConsumer.java
@@ -14,7 +14,7 @@ import org.springframework.util.Assert;
 
 public class AbstractDeadLetterConsumer {
 
-    private String sourceName;
+    private final String sourceName;
     private final Config config;
     protected final Logger logger;
 
