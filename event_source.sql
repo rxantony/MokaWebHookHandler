5a9776f73c719e340d5c1dff6fe61367427662af
Rudi Antony <rudi.antony@mekari.com>
2022-03-10 21:05:33 +0700

    update event_source.sql

--- event_source.sql
+++ event_source.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `event_source` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `data_id` varchar(45) NOT NULL,
-  `data_updated_at` timestamp(6) NOT NULL,
+  `event_date` timestamp(6) NOT NULL,
   `event_name` varchar(45) NOT NULL,
   `payload` text NOT NULL,
   `event_id` varchar(45) NOT NULL,
@@ -11,5 +11,5 @@ CREATE TABLE `event_source` (
   `created_at` timestamp(6) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `data_id_IDX` (`data_id`),
-  KEY `data_id_updated_at_IDX` (`data_id`,`data_updated_at` DESC,`created_at`)
-) ENGINE=InnoDB AUTO_INCREMENT=1449 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `data_id_event_date_IDX` (`data_id`,`event_date` DESC,`created_at`)
+) ENGINE=InnoDB AUTO_INCREMENT=2168 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
