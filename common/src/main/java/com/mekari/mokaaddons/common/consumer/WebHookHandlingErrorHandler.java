38af3d7bec114e6a023441cf6ca5c69be139c58c
Rudi Antony <rudi.antony@mekari.com>
2022-04-05 05:12:56 +0700

    delete UnknownEventFormatException class, update WebHookHandlingErrorHandler class

--- common/src/main/java/com/mekari/mokaaddons/common/consumer/WebHookHandlingErrorHandler.java
+++ common/src/main/java/com/mekari/mokaaddons/common/consumer/WebHookHandlingErrorHandler.java
@@ -1,6 +1,7 @@
 package com.mekari.mokaaddons.common.consumer;
 
 import com.fasterxml.jackson.core.JacksonException;
+import com.mekari.mokaaddons.common.webhook.moka.MokaEventSourceNotFoundException;
 
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.util.ErrorHandler;
@@ -14,6 +15,8 @@ public class WebHookHandlingErrorHandler implements ErrorHandler{
              * if exception is instance of JacksonException, don't reject and requeue it
              */
             if(t.getCause() instanceof JacksonException)
+                return;
+            if(t.getCause() instanceof MokaEventSourceNotFoundException)
                 return;
 
         throw new AmqpRejectAndDontRequeueException(t.getMessage());  
