f7858c2cee5b2b44fb937eedd64b4aded1a26d58
Rudi Antony <rudi.antony@mekari.com>
2022-04-08 16:05:13 +0700

    add beanName length validation

--- common/src/main/java/com/mekari/mokaaddons/common/handler/SpringbootRequestHandlerManager.java
+++ common/src/main/java/com/mekari/mokaaddons/common/handler/SpringbootRequestHandlerManager.java
@@ -10,7 +10,7 @@ import org.springframework.util.Assert;
 public class SpringbootRequestHandlerManager implements RequestHandlerManager {
 
     private ApplicationContext appContext;
-    
+
     public SpringbootRequestHandlerManager(@Autowired ApplicationContext appContext) {
         Assert.notNull(appContext, "appContext");
         this.appContext = appContext;
@@ -23,6 +23,8 @@ public class SpringbootRequestHandlerManager implements RequestHandlerManager {
         var queryHandlerType = ResolvableType.forClassWithGenerics(RawRequestHandler.class,
                 ResolvableType.forClass(requestCls));
         var beanName = appContext.getBeanNamesForType(queryHandlerType);
+        if(beanName.length == 0)
+            throw new IllegalArgumentException("No handler for request:" + request.getClass().getName());
         var queryHandler = (RequestHandler<TRequest, TResult>) appContext.getBean(beanName[beanName.length - 1]);
         return queryHandler.handle(request);
     }
