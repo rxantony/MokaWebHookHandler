631e1d022f728544262d402de96b61fcbcd77e19
Rudi Antony <rudi.antony@mekari.com>
2022-06-11 19:39:14 +0700

    tidy code

--- api/src/main/java/com/mekari/mokaaddons/api/webhook/service/event/command/savepublish/SavePublishEventHandler.java
+++ api/src/main/java/com/mekari/mokaaddons/api/webhook/service/event/command/savepublish/SavePublishEventHandler.java
@@ -19,38 +19,52 @@ import com.mekari.mokaaddons.common.webhook.moka.AbstractEvent;
 
 @Service
 public class SavePublishEventHandler extends AbstractVoidRequestHandler<SavePublishEventRequest> {
-    private @Autowired ObjectMapper mapper;
-    private @Autowired DeadLetterStorage deadLetterStorage;
-    private @Autowired EventSourceStorage eventStorage;
-    private @Autowired Publisher publisher;
-    private @Autowired @Qualifier("save.publish.event") EventNameClassMap eventClsMap;
+
+    private ObjectMapper mapper;
+    private Publisher publisher;
+    private EventSourceStorage eventStorage;
+    private DeadLetterStorage deadLetterStorage;
+    private EventNameClassMap eventClsMap;
+
+    public SavePublishEventHandler(
+            @Autowired @Qualifier("save.publish.event") EventNameClassMap eventClsMap,
+            @Autowired DeadLetterStorage deadLetterStorage,
+            @Autowired EventSourceStorage eventStorage,
+            @Autowired Publisher publisher,
+            @Autowired ObjectMapper mapper) {
+
+        this.eventClsMap = eventClsMap;
+        this.deadLetterStorage = deadLetterStorage;
+        this.eventStorage = eventStorage;
+        this.publisher = publisher;
+        this.mapper = mapper;
+    }
 
     @Override
-    protected void handleInternal(SavePublishEventRequest request) throws Exception{
+    protected void handleInternal(SavePublishEventRequest request) throws Exception {
         String json = null;
         JsonNode jsonNode = null;
-        try{
+        try {
             json = request.getJson();
             jsonNode = mapper.readTree(json);
             var eventName = jsonNode.get("header").get("event_name").asText();
             var eventCls = eventClsMap.gerEventClass(eventName);
-            var event = (AbstractEvent)mapper.readValue(jsonNode.traverse(), eventCls);
+            var event = (AbstractEvent) mapper.readValue(jsonNode.traverse(), eventCls);
             saveEvent(event, request.getJson());
             publishEvent(event);
-        }
-        catch(Exception ex){
-            try{
+        } catch (Exception ex) {
+            try {
                 var builder = BuilderUtil.createDeadLetterStorageItemBuilder(jsonNode)
-                    .payload(request.getJson())
-                    .source(request.getSourceName())
-                    .reason(ex.toString())
-                    .createdAt(DateUtil.utcNow());
+                        .payload(request.getJson())
+                        .source(request.getSourceName())
+                        .reason(ex.toString())
+                        .createdAt(DateUtil.utcNow());
                 deadLetterStorage.insert(builder.build());
             }
             // basically we can forward this exception into ApiExceptionHandler.
-            catch(Exception iex){
+            catch (Exception iex) {
                 logger.error(iex.toString());
-            }           
+            }
         }
     }
 
@@ -62,18 +76,18 @@ public class SavePublishEventHandler extends AbstractVoidRequestHandler<SavePubl
                 header.getEventId(), header.getEventName(), body.getId(), header.getTimestamp().toString());
 
         eventStorage
-            .insert(NewItem
-                    .builder()
-                    .dataId(body.getId().toString())
-                    .eventDate(header.getTimestamp())
-                    .eventName(header.getEventName())
-                    .payload(json)
-                    .eventId(header.getEventId())
-                    .outletId(header.getOutletId())
-                    .version(header.getVersion())
-                    .timestamp(header.getTimestamp())
-                    .createdAt(DateUtil.utcNow())
-                    .build());
+                .insert(NewItem
+                        .builder()
+                        .dataId(body.getId().toString())
+                        .eventDate(header.getTimestamp())
+                        .eventName(header.getEventName())
+                        .payload(json)
+                        .eventId(header.getEventId())
+                        .outletId(header.getOutletId())
+                        .version(header.getVersion())
+                        .timestamp(header.getTimestamp())
+                        .createdAt(DateUtil.utcNow())
+                        .build());
     }
 
     private void publishEvent(AbstractEvent event) {
